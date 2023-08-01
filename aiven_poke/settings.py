197b093d46961a7e41681cbb53cad8507fb7099a
Morten Lied Johansen <morten.lied.johansen@nav.no>
2023-08-01 13:34:34 +0200

    Upgrade to pydantic 2.1.1

--- aiven_poke/settings.py
+++ aiven_poke/settings.py
@@ -1,10 +1,13 @@
-from pydantic import BaseSettings, HttpUrl
+from typing import Optional
+
+from pydantic import HttpUrl
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     aiven_token: str
     main_project: str = "nav-integration-test"
     api_server: str = "http://localhost:8001"
-    webhook_url: HttpUrl = None
+    webhook_url: Optional[HttpUrl] = None
     webhook_enabled: bool = True
-    push_gateway_address: str = None
+    push_gateway_address: Optional[str] = None
