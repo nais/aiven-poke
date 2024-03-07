2d850347ec1120330012742da31ca40227b165dc
Morten Lied Johansen <morten.lied.johansen@nav.no>
2024-03-07 12:17:55 +0100

    First iteration of expiring users message

--- aiven_poke/slack/users.py
+++ aiven_poke/slack/users.py
@@ -1,20 +1,47 @@
+import textwrap
+
 from aiven_poke.slack import Payload, Attachment, Color, Header, Text, TextType, TextSection, \
     FieldsSection
 
+RENEW_DOC = "https://doc.nais.io/how-to-guides/persistence/kafka/renew-credentials-for-non-nais/"
+
+FALLBACK = " ".join(textwrap.dedent("""
+    Your team has users with expiring credentials in the {main_project} pool.
+    Consult the nais documentation to find out how to fix these.
+    The following users needs attention:\n\n 
+"""))
+MAIN_HEADER = "Your team has users in Kafka with expiring credentials"
+WHAT_IS_THIS = " ".join(textwrap.dedent("""
+    Kafka service users are issued certificates with a limited lifespan.
+    When the certificates are getting close to expiring, warnings are issued to avoid interruptions in service.
+    The applications using the affected users needs to get new credentials before the expiry date.
+""").splitlines())
 USERS_HEADER = "*Found service users in the {main_project} pool belonging to team `{team}` with expiring credentials*"
+SOLUTION_HEADER = "*Solution*"
+SOLUTION_PROTECTED = " ".join(textwrap.dedent(f"""
+    For users used by legacy applications not running in the nais platform, consult the documentation on
+    <{RENEW_DOC}|how to renew credentials for non-nais>.
+""").splitlines())
+SOLUTION_NAIS_APP = " ".join(textwrap.dedent("""
+    For users used by applications running in the nais platform, a simple redeploy of your application should be enough.
+""").splitlines())
+NEEDS_HELP = " ".join(textwrap.dedent("""
+    If you need help, reach out in <#C5KUST8N6|nais>
+""").splitlines())
 
 
 def create_user_payload(team, channel, users, main_project):
     users_header = USERS_HEADER.format(main_project=main_project, team=team)
+    usernames = [user.username for user in users]
     return Payload(channel, attachments=[
-        # TODO: Replace FALLBACK with user text
-        Attachment(Color.WARNING, "FALLBACK", blocks=[
-            Header(Text(TextType.PLAIN, "MAIN_HEADER")),  # TODO: Replace MAIN_HEADER with user text
+        Attachment(Color.WARNING, FALLBACK + ", ".join(usernames), blocks=[
+            Header(Text(TextType.PLAIN, MAIN_HEADER)),
+            TextSection(Text(TextType.MRKDWN, WHAT_IS_THIS)),
             TextSection(Text(TextType.MRKDWN, users_header)),
-            FieldsSection(users),
-            # TODO: Replace with correct solutions for expiring users
-            TextSection(Text(TextType.MRKDWN, "SOLUTION_HEADER")),
-            TextSection(Text(TextType.MRKDWN, "SOLUTION1".format(team=team))),
-            TextSection(Text(TextType.MRKDWN, "SOLUTION2")),
+            FieldsSection(usernames),
+            TextSection(Text(TextType.MRKDWN, SOLUTION_HEADER)),
+            TextSection(Text(TextType.MRKDWN, SOLUTION_PROTECTED.format(team=team))),
+            TextSection(Text(TextType.MRKDWN, SOLUTION_NAIS_APP.format(team=team))),
+            TextSection(Text(TextType.MRKDWN, NEEDS_HELP)),
         ])
     ])
